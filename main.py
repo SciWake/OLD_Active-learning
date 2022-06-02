49ff91f497e0f415c161c17079b13c1fef1a0493
Aleksey Kuritsyn <bimastics@gmail.com>
2022-06-03 01:13:35 +0700

    Implement model learning emulation

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ class ModelTraining:
         self.classifier = classifier
         self.train = pd.read_csv(self.path(train_file)).sort_values('frequency', ascending=False)[
             ['phrase', 'subtopic']]
-        self.init_df = self.__init_df('data/input/parfjum_classifier.csv', 'data/model/in_model.csv')
+        self.init_df = self.__init_df('data/input/parfjum_classifier.csv',
+                                      'data/model/in_model.csv')
 
     @staticmethod
     def path(path):
@@ -29,13 +30,12 @@ class ModelTraining:
     def batch(self, batch_size: int) -> pd.DataFrame:
         batch = self.train[:batch_size]
         self.train = self.train[batch_size:]
-        return batch
+        return batch.reset_index()
 
     # There may be data preprocessing or it may be placed in a separate class
-    def update_model(self, batch: pd.DataFrame):
+    def __update_init_df(self, batch: pd.DataFrame):
         self.init_df = pd.concat([self.init_df, batch])
         self.init_df.to_csv(self.path('data/model/in_model.csv'))
-        self.classifier.add(batch['phrase'])
 
     def start(self):
         if not self.classifier.start_model_status:
@@ -47,16 +47,20 @@ class ModelTraining:
             batch = self.batch(batch_size=1000)
             for_training, predict_model = self.classifier.predict(batch['phrase'], 0.95)
             # for_training - индексты объектов где x < limit. Из батча выбираем то, что отправим на разметку
-            self.update_model(batch.loc[for_training])  #
-            # Про
-            for_training, predict_model = self.classifier.predict(batch['phrase'], 0.95)
-            a, p, r = self.classifier.metrics(batch['subtopic'].values, predict_model)
+            self.__update_init_df(batch.loc[for_training])  #
+            # Оцениваем качество модели на всех доступных данных
+            _, predict_model = self.classifier.predict(self.init_df['phrase'], 0.95)
+            a, p, r = self.classifier.metrics(self.init_df['subtopic'],
+                                              self.init_df['subtopic'].values[predict_model])
             metrics['accuracy'].append(a)
             metrics['precision'].append(p)
             metrics['recall'].append(r)
             metrics['batch'].append(
                 batch.shape[0] if len(metrics.get('batch')) == 0 else metrics.get('batch')[-1] +
                                                                       batch.shape[0])
+            # Добавляем новые индексы в модель
+            self.classifier.add(batch['phrase'].loc[for_training])
+        pd.DataFrame(metrics).to_csv('metrics.csv')
 
 
 if __name__ == '__main__':
