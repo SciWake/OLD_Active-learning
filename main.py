aee671d06f7c2e455e68b7cbd5f237b53b416938
Aleksey Kuritsyn <bimastics@gmail.com>
2022-08-08 14:32:23 +0700

    Add a prediction

--- main.py
+++ main.py
@@ -150,7 +150,6 @@ def run_train(train_data_loader, df_train, df_val=None, val_data_loader=None, ep
 
         print(f'Train loss {train_loss} accuracy {train_acc}')
 
-
         # val_acc, val_loss = eval_model(model, val_data_loader, loss_fn, device, len(df_val))
         #
         # print(f'Val loss {val_loss} accuracy {val_acc}')
@@ -164,6 +163,37 @@ def run_train(train_data_loader, df_train, df_val=None, val_data_loader=None, ep
         # if val_acc > best_accuracy:
         #     torch.save(model.state_dict(), 'best_model_state.bin')
         #     best_accuracy = val_acc
+
+
+def get_predictions(model, data_loader):
+    model = model.eval()
+
+    review_texts = []
+    predictions = []
+    prediction_probs = []
+    real_values = []
+
+    with torch.no_grad():
+        for d in data_loader:
+            texts = d["review_text"]
+            input_ids = d["input_ids"].to(device)
+            attention_mask = d["attention_mask"].to(device)
+            targets = d["targets"].to(device)
+
+            outputs = model(input_ids=input_ids, attention_mask=attention_mask)
+            _, preds = torch.max(outputs, dim=1)
+
+            probs = F.softmax(outputs, dim=1)
+
+            review_texts.extend(texts)
+            predictions.extend(preds)
+            prediction_probs.extend(probs)
+            real_values.extend(targets)
+
+    predictions = torch.stack(predictions).cpu()
+    prediction_probs = torch.stack(prediction_probs).cpu()
+    real_values = torch.stack(real_values).cpu()
+    return review_texts, predictions, prediction_probs, real_values
 
 
 from sklearn.preprocessing import LabelEncoder
